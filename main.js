464418c828cea547d63041e752bacc96ae19e9fe
Mostafa S. El-bastawisy <mostafa.sayed112@gmail.com>
2021-08-04 14:44:54 +0200

    Preparing 2

--- main.js
+++ main.js
@@ -21,19 +21,12 @@ for ( let i = 0; i < x ; i++ ) {
   document.write(`<h3>Team of Members :</h3>`);
   for( let j = 0; j < myEmployees.length; j++ ) {
     document.write(`<h4> ${j + 1} - ${myEmployees[j]}</h4>`);
-  }
-  // for ( let k = 0 ; k < firsAdmin ; k++){
-  //     for ( let l = 0 ; l < firsEmp ; l++);
-  //     if( firsAdmin[k] === firsEmp[l]);
-      
-  // }
-    
-    
+  }  
   document.write(`<hr>`)
   document.write(`</div>`);
 }
 
-//---------------------//
+console.log("########################");
 
 let start = 10;
 let end = 100;
@@ -43,8 +36,75 @@ for (let i = start; i <= end; i += start) {
 	if (i !== exclude) {
 		console.log(i);
 	}
+};
+
+console.log("########################");
+
+let start1 = 10;
+let end1 = 0;
+let stop1 = 3;
+
+for (let k = start1; k >= stop1 ; k--) {
+	if (k < start1) {
+		console.log(`0${k}`);
+	} else {
+		console.log(k);
+	}
 }
 
+console.log("########################");
+
+let start2 = 1;
+let end2 = 6;
+let breaker1 = 2;
+
+for (let j = start2; j <= end2 ; j++){
+  console.log(j);
+  console.log(`-- ${breaker1}`);
+	console.log(`-- ${breaker1 + breaker1}`);
+} 
+
+console.log("########################");
+
+let index = 10;
+let jump = 2;
+let end3 = 0;
+
+for (;;) {
+  if (index > jump) {
+		console.log(index);
+		index -= jump;
+	} else break;
+  
+}
+
+console.log("########################");
+
+let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
+let letter = "a";
+let value = [];
 
 
+for (let a = value.length ; a < friends.length ; a++ ) {
+  if (!friends[a].toLowerCase().startsWith(letter)){
+    value.push(friends[a])
+  }
+}
+for (let i = Number(!Array.isArray(friends)); i < value.length; i++) {
+    console.log(`${i + letter.length} => ${value[i]}`);
+  }
 
+console.log("########################");
+
+let start4 = 0;
+let swappedName = "elZerO";
+
+for (let f = start4; f < swappedName.length ; f++){
+  if (swappedName[f].toLowerCase()){
+    swappedName.toUpperCase()
+  }
+  
+  console.log(swappedName[f])
+}
+
+console.log("########################");
